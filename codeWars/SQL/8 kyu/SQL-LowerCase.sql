143c84aa1d2e041d18a6f2d39f69d06b684543d0
Artem Sankov <artemsankov0000@gmail.com>
2023-11-12 16:44:36 +0300

    sql: task sql lower case done

---
+++ codeWars/SQL/8 kyu/SQL-LowerCase.sql
@@ -0,0 +1,12 @@
+-- TASK
+-- Given a demographics table in the following format:
+
+-- ** demographics table schema **
+
+-- id
+-- name
+-- birthday
+-- race
+-- you need to return the same table where all letters are lowercase in the race column.
+
+SELECT id, name, birthday, LOWER(race) "race" from demographics 
