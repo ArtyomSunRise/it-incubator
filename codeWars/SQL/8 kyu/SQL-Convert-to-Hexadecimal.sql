5ab9d6c44b679793f0a3b9b69aa5999609854041
Artem Sankov <artemsankov0000@gmail.com>
2023-11-12 16:38:42 +0300

    sql: task sql convert to hexadecimal done

---
+++ codeWars/SQL/8 kyu/SQL-Convert-to-Hexadecimal.sql
@@ -0,0 +1,17 @@
+-- TASK
+
+-- to hexYou have access to a table of monsters as follows:
+
+-- monsters table schema
+-- id
+-- name
+-- legs
+-- arms
+-- characteristics
+-- Your task is to turn the numeric columns (arms, legs) into equivalent hexadecimal values.
+
+-- output table schema
+-- legs
+-- arms
+
+SELECT to_hex(arms) arms, to_hex(legs) legs FROM monsters
